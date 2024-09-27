def9f9edcf17b12afb59d5c0f879edcc9fc233d4
Phalcin <Emmanuelhexer@gmail.com>
2024-09-27 06:00:17 +0000

    new changes

--- index.py
+++
@@ -1,3 +0,0 @@
-list1 = ["a", "b", "c"]
-list1.append("d")
-print(list1)
--- script.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector("button");
-const navBar = document.querySelector("section");
-
-btn.addEventListener("click", () => {
-  <li>
-    <a href="#">About</a>
-  </li>;
-  navBar.classList.add("move-down");
-});
